0175a3b9b2077095cd820ee587d5c1eaeef5a203
ZhaoXuChen <2250970846@qq.com>
2017-08-04 12:05:38 +0800

    Vr_Net_Server_Unity3D
    The primary codes

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        //数据类型编码算法
+        //将int转为低字节在前，高字节在后的byte数组
+        public static byte[] InttoByteArray(int n)
+        {
+            byte[] b = new byte[4];
+            b[0] = (byte)(n & 0xff);
+            b[1] = (byte)(n >> 8 & 0xff);
+            b[2] = (byte)(n >> 16 & 0xff);
+            b[3] = (byte)(n >> 24 & 0xff);
+            return b;
+        }
+
+        //将short转为低字节在前，高字节在后的byte数组
+        public static byte[] ShorttoByteArray(short n)
+        {
+            byte[] b = new byte[2];
+            b[1] = (byte)(n & 0xff);
+            b[0] = (byte)(n >> 8 & 0xff);
+            return b;
+        }
+
+        //将int类型网络字节转化为主机字节算法
+        public static int ByteArraytoInt(byte[] b)
+        {
+            int iOutcome = 0;
+            byte bLoop;
+            for (int i = 0; i < 4; i++)
+            {
+                bLoop = b[i];
+                iOutcome += (bLoop & 0xff) << (8 * i);
+            }
+            return iOutcome;
+        }
+
+        //将short类型的网络字节转化为主机字节算法
+        private static short ByteArraytoShort(byte[] b)
+        {
+            short iOutcome = 0;
+            byte bLoop;
+            for (int i = 0; i < 2; i++)
+            {
+                bLoop = b[i];
+                iOutcome += (short)((bLoop & 0xff) << (8 * i));
+            }
+            return iOutcome;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static byte[] bt = new byte[1024];
+        static bool isrec = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("server start: ....");
+            string ip = "127.0.0.1";
+            byte[] buffer = new byte[1024];
+
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            Socket a = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint ipe = new IPEndPoint(ipAddress, 6666);
+            a.Bind(ipe);
+            a.Listen(5);
+            Socket b = a.Accept();
+
+
+            Console.WriteLine("accept a client: ....");
+
+            Thread pp = new Thread(Program.sendmessage);
+            pp.Start(b);
+
+
+            for (int i = 10; i < 30; i++)
+            {
+                Console.WriteLine(i);
+                byte[] position = Class1.InttoByteArray(i);
+                try
+                {
+                    b.Send(position);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            Thread newsendmsg = new Thread(Program.sendmsg);
+            newsendmsg.Start(b);
+
+            Console.ReadKey();
+            b.Close();
+            a.Close();
+        }
+
+        public static void sendmessage(object s)
+        {
+            isrec = true;
+            Socket b = (Socket)s;
+            int rec = b.Receive(bt, 4, 0);
+            int recint = Class1.ByteArraytoInt(bt);
+            Console.WriteLine(recint);
+            Thread.Sleep(50);
+        }
+
+        public static void sendmsg(object s)
+        {
+            Socket b = (Socket)s;
+
+            while (true)
+            {
+                if (!isrec)
+                {
+                    try
+                    {
+                        b.Send(bt);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                if(!b.Connected)
+                {
+                    Console.WriteLine("client is lost");
+                }
+            }
+        }
+    }
+}
